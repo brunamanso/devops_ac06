53495b25c2c49124fd3fd2b7276b083c6c9098ca
pablotwd08 <pablo-twd08@hotmail.com>
2019-10-03 22:11:10 -0300

    test6

--- com/kuma/test/test_conta_corrente.py
+++ com/kuma/test/test_conta_corrente.py
@@ -1,16 +1,5 @@
 """arquivo de calculo de parcela"""
-def test_alterarN():
-    conta_corrente.alterarNome('Luis')
-    assert conta_corrente.nomeCorrentista == 'Luis'
 
-
-"""arquivo de calculo de parcela"""
-def test_deposito():
-    conta_corrente.deposito(100)
-    assert conta_corrente.saldo == 100
-
-
-"""arquivo de calculo de parcela"""
-def test_saque():
-    conta_corrente.saque(100)
-    assert conta_corrente.saldo == 0
+cont = 0
+soma = cont + 1
+print(soma)
--- com/kuma/test/test_converte_hora.py
+++ com/kuma/test/test_converte_hora.py
@@ -1,21 +1,5 @@
-from com.kuma.converte_hora import converte_hora
-
-
 """arquivo de calculo de parcela"""
-def test_converte_hora_none():
-	assert converte_hora(24, 70) is None, "should be none"
 
-
-"""arquivo de calculo de parcela"""
-def test_converte_hora_zero():
-	assert converte_hora(0, 22) == '12:22 AM', "should be 12"
-
-
-"""arquivo de calculo de parcela"""
-def test_converte_hora_12():
-	assert converte_hora(10, 30) == "10:30 AM", "should be 10:30 AM"
-
-
-"""arquivo de calculo de parcela"""
-def test_converte_hora_24():
-	assert converte_hora(13, 13) == "01:13 PM", "should be 12"
+cont = 0
+soma = cont + 1
+print(soma)
