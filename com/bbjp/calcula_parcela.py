15c02fd2e3eba2c7490d3f6d102a88f3aedaa208
pablotwd08 <pablo-twd08@hotmail.com>
2019-10-03 21:28:41 -0300

    certo

--- com/bbjp/calcula_parcela.py
+++
@@ -1,8 +0,0 @@
-def valor_pagamento(valor, dias_atraso):
-    if valor < 0:
-        return None
-    if dias_atraso > 0:
-        multa = valor * 0.03
-        adicional_atraso = valor * (dias_atraso * 0.01)
-        return valor + multa + adicional_atraso
-    return valor
